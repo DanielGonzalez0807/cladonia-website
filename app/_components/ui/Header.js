d78aebfaad45596cd6fb2281ce337de310ab2852
Daniel González <danielgo.softdev@gmail.com>
2026-03-11 23:07:44 -0500

    Correción al header, espaciado responsive para movil

--- app/_components/ui/Header.js
+++ app/_components/ui/Header.js
@@ -25,7 +25,7 @@ export default function Header() {
       animate="show"
       className="fixed top-0 left-0 w-full z-50 bg-linear-to-r from-black/99 from-28% lg:from-10% via-black/0 via-46% lg:via-22% to-black/10 shadow-2xl shadow-black/20"
     >
-      <nav className="relative mx-auto max-w-au px-4 md:px-8 py-2 md:py-3 flex items-center justify-between bg-black/16 backdrop-blur-md shadow-[0_10px_30px_rgba(0,0,0,0.12)] ">
+      <nav className="relative mx-auto max-w-au px-2 md:px-8 py-2 md:py-3 flex items-center justify-between bg-black/16 backdrop-blur-md shadow-[0_10px_30px_rgba(0,0,0,0.12)] ">
         {/* Logo */}
         <Link href="/" className="flex flex-row items-center">
           <Image
@@ -42,12 +42,12 @@ export default function Header() {
             alt="Nombre del proyecto"
             width={150}
             height={30}
-            className="relative object-contain invert md:w-50 md:h-10 mx-3 shadow-2xl shadow-black"
+            className="relative object-contain invert md:w-50 md:h-10 mx-1 lg:mx-3 shadow-2xl shadow-black"
           />
         </Link>
 
         {/* navegación */}
-        <div className="flex items-center gap-4 lg:gap-12 ">
+        <div className="flex items-center gap-2 lg:gap-12 ">
           <a
             href="#contacto"
             className="text-white hover:text-white/90 transition text-lg font-medium text-shadow-md text-shadow-black/50"
