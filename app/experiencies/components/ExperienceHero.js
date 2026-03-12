2af060cbcd4208f982add28c711b831a66f97c8a
Daniel González <danielgo.softdev@gmail.com>
2026-03-11 23:19:04 -0500

    Corrección responsive para hero experiencies

--- app/experiencies/components/ExperienceHero.js
+++ app/experiencies/components/ExperienceHero.js
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import Header from "./Header";
+import Image from 'next/image';
+import Header from "@/app/_components/ui/Header";
 
 export default function ExperienceHero({ data, slug }) {
   return (
-    <section className="relative h-screen w-screen overflow-hidden">
-
-
+    <section className="relative h-[80vh] w-screen overflow-hidden">
       <Image
         src="/images/og-image.jpg"
         alt="Hero background"
@@ -14,27 +12,31 @@ export default function ExperienceHero({ data, slug }) {
         priority
       />
 
-      {/* HEADER (fixed, ya definido internamente) */}
+      {/* overlay para mejorar contraste */}
+      <div className="absolute inset-0 bg-linear-to-t from-black/70 via-black/40 to-black/10" />
+
+      {/* HEADER */}
       <Header />
 
       {/* CONTENIDO */}
-      <div className="relative z-10 h-full flex flex-col justify-center items-center pt-24">
-        <div className="text-center max-w-7xl mx-auto px-6 pb-20 text-white">
-          <h2 className="text-4xl md:text-6xl font-medium mb-4 tracking-widest drop-shadow-lg">
+      <div className="relative z-10 h-full flex flex-col justify-center items-center pt-20 md:pt-24">
+        <div className="text-center max-w-5xl mx-auto px-6 text-white">
+          <h2 className="text-lg md:text-3xl lg:text-4xl font-medium mb-3 md:mb-4 tracking-widest drop-shadow-lg">
             {data.subtitle}
           </h2>
 
-          <h1 className="text-8xl md:text-9xl font-bold leading-tight drop-shadow-lg">
-            {data.title.split(" ").pop().toUpperCase()}
+          <h1 className="text-5xl md:text-7xl lg:text-9xl font-bold leading-tight drop-shadow-lg">
+            {data.title.split(' ').pop().toUpperCase()}
           </h1>
 
-          <p className="mt-4 max-w-300 text-2xl drop-shadow-lg text-balance">
-            {data.hero.hectares} hectáreas de páramo y bosque altoandino te esperan.
+          <p className="mt-4 max-w-xl md:max-w-4xl mx-auto text-base md:text-xl lg:text-2xl drop-shadow-lg text-balance mb-16">
+            {data.hero.hectares} hectáreas de páramo y bosque altoandino te
+            esperan.
+            <br></br>
             Desde {data.hero.founded} protegiendo la biodiversidad de Colombia.
           </p>
         </div>
       </div>
-
     </section>
   );
 }
